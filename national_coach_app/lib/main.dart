0373df210851a48edf41fd7ad74acc6796da4af9
Maccrey_MacBook Air <maccrey@naver.com>
2024-08-30 20:46:48 +0900

    라이트테마&다크테마 설정

--- national_coach_app/lib/main.dart
+++ national_coach_app/lib/main.dart
@@ -1,23 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:national_coach_app/Controller/theme_controller.dart';
 import 'package:national_coach_app/Screen/home_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  // ThemeController 인스턴스를 생성하여 사용합니다.
+  final ThemeController themeController = Get.put(ThemeController());
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'National Coach',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const HomeScreen(),
-    );
+    return Obx(
+        // 테마가 변경될 때마다 Obx가 반응하여 UI를 업데이트 합니다.
+        () => GetMaterialApp(
+              title: 'Flutter Demo',
+              theme: themeController.isDarkMode.value
+                  ? ThemeData.dark()
+                  : ThemeData.light(),
+              home: const HomeScreen(),
+            ));
   }
 }
